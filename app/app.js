d404db54bf93ead8d899159c86ad8d855c64da9e
riostrebleh <helbert.v.rios@gmail.com>
2018-10-28 00:57:16 +0000

    Rodando a aplicação.

--- app/app.js
+++ app/app.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var app = express();
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 0;
-
-console.log('process.env: ' + JSON.stringify(process.env));
-console.log('port: ' + port);
-console.log('ip: ' + ip);
-
-
-
-app.listen(port, ip, function (err) {
-    console.log('Running on port ' + port + ' ip: ' + ip);
+ 
+var ports = [3000, 8080];
+ 
+ports.forEach(port => {
+    app.listen(port, function (err) {
+        console.log('Running on port '+port+'.');
+    });
 });
+ 
 
 app.get('/', function(req, res) {
-    res.send('Hello Word!');
+    res.send("Helbert say: Hello World!");
 });
